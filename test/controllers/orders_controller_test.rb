2c2a94d3d3112e7900b1df7eff5ebd18f0a50bb6
matthewdillender <156735514+matthewdillender@users.noreply.github.com>
2024-05-07 20:02:51 -0700

    add controllers/tests

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -1,7 +1,49 @@
 require "test_helper"
 
 class OrdersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  require 'test_helper'
+
+class OrdersControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:one)
+    @order = orders(:one)
+  end
+
+  test "should get index" do
+    sign_in @user
+    get orders_url
+    assert_response :success
+  end
+
+  test "should create order" do
+    sign_in @user
+    assert_difference('Order.count') do
+      post orders_url, params: { order: { total_price: @order.total_price, status: @order.status } }
+    end
+
+    assert_response 201
+  end
+
+  test "should show order" do
+    sign_in @user
+    get order_url(@order)
+    assert_response :success
+  end
+
+  test "should update order" do
+    sign_in @user
+    patch order_url(@order), params: { order: { total_price: @order.total_price, status: @order.status } }
+    assert_response 200
+  end
+
+  test "should destroy order" do
+    sign_in @user
+    assert_difference('Order.count', -1) do
+      delete order_url(@order)
+    end
+
+    assert_response 204
+  end
+end
+
 end
