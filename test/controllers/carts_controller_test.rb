7f138d3b0ef0209840cac59a6160ac8ccd4f79d5
matthewdillender <156735514+matthewdillender@users.noreply.github.com>
2024-05-07 20:06:17 -0700

    remove tests

--- test/controllers/carts_controller_test.rb
+++ test/controllers/carts_controller_test.rb
@@ -1,37 +1,7 @@
 require "test_helper"
 
 class CartsControllerTest < ActionDispatch::IntegrationTest
-  require 'test_helper'
-
-  class CartsControllerTest < ActionDispatch::IntegrationTest
-    setup do
-      @user = users(:one)
-      @cart = carts(:one)
-    end
-  
-    test "should show cart" do
-      sign_in @user
-      get cart_url
-      assert_response :success
-    end
-  
-    test "should create cart" do
-      sign_in @user
-      assert_difference('Cart.count') do
-        post carts_url, params: { cart: { user_id: @cart.user_id, session_id: @cart.session_id } }
-      end
-  
-      assert_response 201
-    end
-  
-    test "should destroy cart" do
-      sign_in @user
-      assert_difference('Cart.count', -1) do
-        delete cart_url
-      end
-  
-      assert_response 204
-    end
-  end
-  
+  # test "the truth" do
+  #   assert true
+  # end
 end
