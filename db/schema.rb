13614d142daa6975a9e49b311f1d62d426520e74
matthewdillender <156735514+matthewdillender@users.noreply.github.com>
2024-05-07 19:39:53 -0700

    add column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_08_022132) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_08_023917) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -53,6 +53,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_08_022132) do
     t.string "availability"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id"
   end
 
   create_table "users", force: :cascade do |t|
